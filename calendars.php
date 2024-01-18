d08a660b82edf8832c37ad442ec8c8ecde53a892
sudhakarvita <sudhakar.vita99@gmail.com>
2024-01-18 15:08:39 +0530

    small change in calendar

--- calendars.php
+++ calendars.php
@@ -12,39 +12,46 @@
             <tr style="color:#000000;font-family:calibri;font-size:18px">
                 <th>S.No.</th>
                 <th>Name of the Courses</th>
+                <th>Academic Year</th>
                 <th class="text-center">Academic calender</th>
                       </tr>
                        <tr style="color:#000000;">
                 <td>1</td>
                 <td>B.Tech I Year II Semester Revised Academic Calendar for AY 2022-23</td>
+                <td class="text-center">2023-24</td>
                 <td class="text-center"><a href="files/calendar/B.Tech_._I_Year_II_Semester_Revised_Academic_Calendar_for_AY_2022-23_.pdf" target="_new">View</a></td>
                       </tr>
                        <tr style="color:#000000;">
                 <td>2</td>
                 <td>B.Tech/B.Pharm.III YEAR I & II SEMESTERS</td>
+                <td class="text-center">2023-24</td>
                 <td class="text-center"><a href="files/calendar/B.Tech.B.Pharm.IIIYEARIIISEMESTERS.pdf" target="_new">View</a></td>
                 
                       </tr>
                        <tr style="color:#000000;">
                 <td>3</td>
                 <td>B.Tech II YEAR I & II SEMESTERS</td>
+                <td class="text-center">2023-24</td>
                 <td class="text-center"><a href="files/calendar/B.TechIIYEARIIISEMESTERS.pdf" target="_new">View</a></td>
                 
                       </tr>
                        <tr style="color:#000000;">
                 <td>4</td>
                 <td>MBA and MCA II YEAR I & II SEMESTERS</td>
+                <td class="text-center">2023-24</td>
                 <td class="text-center"><a href="files/calendar/MBAandMCAIIYEARIIISEMESTERS.pdf" target="_new">View</a></td>
                
                       </tr>
                       <tr style="color:#000000;">
                 <td>5</td>
                 <td>MBA MCA I YEAR I & II SEMESTERS</td>
+                <td class="text-center">2023-24</td>
                 <td class="text-center"><a href="files/calendar/MBAMCA_I_YEAR_I_II_SEMESTERS.pdf" target="_new">View</a></td>
                       </tr>
                       <tr style="color:#000000;">
                 <td>6</td>
                 <td>Revised B.Tech & B.Pharm IV YEAR I & II Semesters Academic Calendar for 2023-24</td>
+                <td class="text-center">2023-24</td>
                 <td class="text-center"><a href="files/calendar/Revised_B._Tech_.B_._Pharm_._IV_YEAR_I_II_Semesters_Academic_Calendar_for_2023-24_.pdf" target="_new">View</a></td>
                       </tr>
         </table>
