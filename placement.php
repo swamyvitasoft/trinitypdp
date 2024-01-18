088bf1868cf5880786fca156d82aebbe55705b78
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-01-18 22:33:28 +0530

    changes in placement files by dinesh

---
+++ placement.php
@@ -0,0 +1,162 @@
+<?php include 'header.php'; ?>
+<div class="row">
+    <div class="eee" style="background-image: url('images/placement/bgpic.png'); background-size:cover; height:200px;">
+    </div>
+</div>
+<div class="container-fluid" style="box-shadow:-3px 5px 15px #000">
+    <ul class="nav nav-tabs navv" role="tablist" style="background-color: #f7f7f7" ;>
+        <li class="nav-item" style="color: rgb(0, 0, 255);">
+            <a class="nav-link active" data-toggle="tab" href="#home"><b style="font-size:16px;color:rgb(120, 120, 120)">Placement cell</b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu1"><b style="font-size:16px;color:rgb(120, 120, 120)">Skill enhancement Program</b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu2"><b style="font-size:16px;color:rgb(120, 120, 120)">Task</b></a>
+        </li>
+        <!-- <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu3"><b style="font-size:16px;color:rgb(120, 120, 120)">Infosys Campus Connect</b> </a>
+        </li> -->
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu4"><b style="font-size:16px;color:rgb(120, 120, 120)">Personal Counselling</b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu5"><b style="font-size:16px;color:rgb(120, 120, 120)">Students Placed</b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu6"><b style="font-size:16px;color:rgb(120, 120, 120)">Placements Gallery </b> </a>
+        </li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+    <div id="home" class="container tab-pane fade">
+            <h2 style="color: #003366;">Placement Cell</h2><br>
+            <p>
+            Images to add here
+             </P>
+        </div>
+        <div id="menu1" class="container tab-pane fade" style="color:#082b2a;">
+            <h2 style="color: #003366;">SKILL</h2>
+            <hr>
+            <!-- <td><img src="images/faculty/1.jpeg" style="margin-left: 40%; height=" 120" width="100" /></td> -->
+            <p style="text-indent: 50px;" align:"justify">
+            Most of the students at Trinity college of engineering have come from a rural background and their language skills and employability skills are not up to industry requirement and moreover skill set is evolving rapidly. To cope up with the rapid change, students are trained in specific areas that will help them to explore new knowledge, attitude, and skills, both personally and professionally.</p><br>
+            <p style="text-indent: 50px;" align:"justify">Hence the skill Enhancement Program was introduced at TCEK. Under this program, our students undergo training and learning of about 6 hours in a week. The students undergo training in Personality Development Exercises, Managing Emotions, Time Management, and Stress Management. They are also trained and given opportunities to enhance their skills like public speaking, presentation skills, body language and preparation of reports </p>
+        </div>
+        <div id="menu2" class="container tab-pane fade">
+            <h2 style="color: #003366;">TASK</h2><br>
+            <p>
+            Telangana Academy for Skill & Knowledge was established by the Government of Telangana to enable a platform between Government, Academia, and Industry to enhance employability quotient of youth in the state.</P>
+
+<p>TASK is not a profit-oriented organization but its created by the Government of Telangana for bringing synergy among institutions of Government, Industry & Academia with an objective of offering quality human resources and services to the industry. Granting access to modules for enhancing their technology, personal and organizational skills at highly subsidized rates. Partnerships with colleges to create environs conducive for growth through faculty development, research pilots and help colleges to provide quality education for the leaders of tomorrow with focused systematic Programme. TASK programs help corporates gain access to a pool of trained graduates for suitable roles. Our students are trained extensively in the latest technology to help companies find the right candidate.
+            </p>
+        </div>
+        <div id="menu3" class="container tab-pane fade">
+            <h2 style="color: #003366;">Infosys Campus Connect</h2><br>
+            <p>
+         Infosys images to add here
+            </p>
+        </div>
+        <div id="menu4" class="container tab-pane fade">
+            <h2 style="color: #003366;">Personal Counselling</h2><br>
+            <p>
+            The aim of providing personal counselling is to facilitate comprehensive guidance programmes that equip the students with necessary attitude, knowledge, behaviour and skills to become sensible and responsible individuals. Personal counselling allows individuals to explore their feelings, behaviours, beliefs and help them to work through challenging or influential memories. It also helps an individual to identify aspects of their lives that they can change for their betterment, set personal goals and work towards desired changes. </p>
+
+            <p>The primary goal to provide counselling session to the students at Trinity college of engineering is to help individuals build upon their strengths and to minimize their weakness and enhance their ability to address their issues of life and personal adjustment. The students who have inferiority complex are also counselled and they are suggested to build self confidence. Special observation is made towards identified the change in their attitude.</p>
+
+            <p>The counsellor clarifies nature of concern, offers supports, encourages a deeper understanding of issues, and provides information and guidelines a work towards a problem – solving solution. The personal counselling is done for 250 students on various issues like improper behaviour, irregularity to college, not being punctual to college, being poor in academics subjects etc. in the Academic year 2018-19 making TCEK is the best college in peddapelli.
+            </p>
+        </div>
+        <div id="menu5" class="container tab-pane fade">
+            <h2 style="color: #003366;">Students placed</h2><br>
+     <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student1.jpeg" alt="1st" width="100%" height="400px" style="padding:20px">
+    </div>
+     <div class="col-sm-6">
+    <img src="images/placement/student2.jpeg" alt="2nd" width="100%" height="400px"style="padding:20px">
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student3.jpeg" alt="3th" width="100%" height="400px"style="padding:20px">
+     </div>
+     <div class="col-sm-6">
+    <img src="images/placement/student4.jpeg" alt="4nd" width="100%" height="400px"style="padding:20px">
+     </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student5.jpeg" alt="5rd" width="100%" height="400px"style="padding:20px">
+     </div>
+     <div class="col-sm-6">
+    <img src="images/placement/student6.jpeg" alt="6th" width="100%" height="400px"style="padding:20px">
+     </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student7.jpeg" alt="7th" width="100%" height="400px"style="padding:20px">
+     </div>
+     <div class="col-sm-6">
+    <img src="images/placement/student8.jpeg" alt="8th" width="100%" height="400px"style="padding:20px">
+     </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student9.jpeg" alt="9th" width="100%" height="400px"style="padding:20px">
+     </div>
+     <div class="col-sm-6">
+    <img src="images/placement/student11.jpeg" alt="11th" width="100%" height="400px"style="padding:20px">
+     </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student12.jpeg" alt="12th" width="100%" height="400px"style="padding:20px">
+     </div>
+     <div class="col-sm-6">
+    <img src="images/placement/student13.jpeg" alt="13th" width="100%" height="400px"style="padding:20px">
+     </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+    <img src="images/placement/student15.jpeg" alt="15th" width="100%" height="400px"style="padding:20px">
+     </div>
+    </div>
+   
+        
+        </div>
+
+        <div id="home" class="container tab-pane fade">
+            <div>
+            <div class="col-sm-8">
+                    <img src="images/placement/img1.jpeg" alt="" width="100%" height="350" style="padding:10px">
+                </div>
+            </div>
+        </div>
+
+        <div id="menu3" class="container tab-pane fade">
+            <!--  <h3>Menu 3</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>-->
+        </div>
+        <div id="menu4" class="container tab-pane fade">
+            <!--  <h3>Menu 4</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>-->
+        </div>
+        <div id="menu5" class="container tab-pane fade">
+
+
+        </div>
+        <div id="menu6" class="container tab-pane fade">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <img src="images/placement/img1.jpeg" alt="" width="100%" height="350" style="padding:10px">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br>
+<?php include 'footer.php'; ?>
