3ca75e576e9759e207d8eb4d9ff00a7e3a9f1907
srikanthyaswada <srikanthyaswada.vita@gmail.com>
2024-01-20 11:21:10 +0530

    eee page changes completed

--- header.php
+++ header.php
@@ -100,15 +100,38 @@ $highlighted = $url[0][2];
 <body>
     <!-- START SITE -->
     <div id="wrapper" style=" overflow: hidden;">
+        <div class="row " style="background-color: #07294d;padding-top:-15px;padding-bottom:-10px">
+                        <div class="col-sm-8"></div>
+                        <div class="col-sm-4">
+                            <div id="navbar" class="navbar-collapse collapse">
+                                <ul class="nav navbar-nav">
+                                    <li class="dropdown has-submenu">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="color:white;font-size:15px;padding-top:0px">Committees</span> <span class="fa fa-angle-down"></span></a>
+                                        <ul class="dropdown-menu" style="line-height:2.5">
+                                            <li><a href="osg.php">Online Student Grievance </a></li>
+                                            <li><a href="osg.php">College Disciplinary </a></li>
+                                            <li><a href="anti.php">Anti-Ragging </a></li>
+                                            <li><a href="gri.php">Grievance Redressal </a></li>
+                                            <li><a href="women.php">Women Protection Cell</a></li>
+                                            <li><a href="scst.php">SC & ST Welfare</a></li>
+                                            <li><a href="Grievance Redressal.php">Ombudsperson Details</a></li>
+                                            <li><a href="gove body.php">Governing Body</a></li>
+                                            <li><a href="Grievance Redressal.php">MANDATORY DISCLOSURE</a></li>
+                                            <li><a href="Grievance Redressal.php">Alumni</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown has-submenu <?= ($highlighted == 'admission.php') ? 'active' : ''; ?>">
+                                        <a href="admission.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="color:white;font-size:15px">Admissions</span><span class="fa fa-angle-down"></span></a>
+                                    </li>
+                                    <li class="dropdown <?= ($highlighted == 'contact.php') ? 'active' : ''; ?>"><a href="contact.php"><span style="color:white;font-size:15px">Contact</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+        </div> <!--end .. row -->    
+
         <div class="header-style-2">
             <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-11 col-sm-11 nopad">
-                        <marquee style="float: right; " onmouseover="this.stop();" onmouseout="this.start();">
-                            <h5 style="color: #07294d;"><b class="contact">For Admissions Contact : 8522954369, 7396903383</b></h5>
-                        </marquee>
-                    </div>
-                    <div class="col-md-1 col-sm-1 nopad"><a href="contact.php" style="background-color:#07294d;color: white;" class="btn btn-primary">Contact US</a></div>
+                <div class="row" >    
                     <div class="col-md-12 col-sm-12 nopad">
                         <header class="header">
                             <nav class="yamm navbar navbar-default">
@@ -123,7 +146,7 @@ $highlighted = $url[0][2];
                                         <a class="" href="index.php"><img src="images/logo1.png" class=" main-Img" alt="" style="width:200px"></a>
                                     </div>
                                     <div id="navbar" class="navbar-collapse collapse">
-                                        <ul class="nav navbar-nav" style="padding-left: 50px;">
+                                        <ul class="nav navbar-nav" style="padding-left:50px;">
                                             <li class="dropdown has-submenu <?= ($highlighted == 'index.php') ? 'active' : ''; ?>">
                                                 <a href="index.php">Home</a>
                                             </li>
@@ -149,8 +172,8 @@ $highlighted = $url[0][2];
                                                             <li><a href="diploma.php">Diploma</a></li>
                                                         </ul>
                                                     </li>
-                                                    <li><a href="#">Academic Calendars</a></li>
-                                                    <li><a href="#">Academic Regulations</a></li>
+                                                    <li><a href="calendars.php">Academic Calendars</a></li>
+                                                    <li><a href="regulation.php">Academic Regulations</a></li>
                                                     <!--  <li><a href="admission.php">Admissions </a></li>-->
                                             </li><!-- end li -->
                                         </ul><!-- end ul -->
@@ -162,7 +185,7 @@ $highlighted = $url[0][2];
                                                 <li><a href="ece.php">ECE</a></li>
                                                 <li><a href="cse.php">CSE</a></li>
                                                 <li><a href="aiml.php">AIML</a></li>
-                                                <li><a href="hs.php">H & S</a></li>
+                                                <!-- <li><a href="hs.php">H & S</a></li> -->
                                                 <li><a href="mba.php">MBA</a></li>
                                                 <li><a href="#">Diploma in EEE</a></li>
                                                 <li><a href="#">Diploma in ECE</a></li>
@@ -172,8 +195,8 @@ $highlighted = $url[0][2];
                                         <li class="dropdown has-submenu <?= ($highlighted == 'selected.php' || $highlighted == 'tpo.php') ? 'active' : ''; ?>">
                                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Placement Cell <span class="fa fa-angle-down"></span></a>
                                             <ul class="dropdown-menu">
-                                                <li><a href="selected.php">Selected students</a></li>
-                                                <li><a href="#">Companies Visited</a></li>
+                                                <li><a href="placement.php">Placement Cell</a></li>
+                                                <!-- <li><a href="#">Companies Visited</a></li>/s -->
                                                 <li><a href="tpo.php">Contact TPO</a></li>
                                             </ul>
                                         </li>
@@ -183,7 +206,7 @@ $highlighted = $url[0][2];
                                                 <li><a href="library.php">Library</a></li>
                                                 <li><a href="hostel.php">Hostel</a></li>
                                                 <li><a href="transport.php">Transport</a></li>
-                                                <li><a href="wifi.php">Wi-Fi</a></li>
+                                                <!-- <li><a href="wifi.php">Wi-Fi</a></li> -->
                                                 <li><a href="key.php">Key-Facilities</a></li>
                                                 <li><a href="#">E-learning and Internet</a></li>
                                                 <li><a href="#">Sports </a></li>
@@ -216,7 +239,6 @@ $highlighted = $url[0][2];
                                                 <li><a href="C5DVV.PHP">CRITERIA-5</a></li>
                                                 <li><a href="C6DVV.PHP">CRITERIA-6</a></li>
                                                 <li><a href="C7DVV.PHP">CRITERIA-7</a></li>
-
                                             </ul>
                                         </li>
                                         <!-- <li class="dropdown <?= ($highlighted == 'contact.php') ? 'active' : ''; ?>"><a href="contact.php">Contact</a></li> -->
@@ -226,10 +248,18 @@ $highlighted = $url[0][2];
                             </nav>
                         </header><!-- end header -->
                     </div>
-                    <div class="col-md-12 col-sm-12" style="background-color: #07294d;margin-top:-20px">
-                        <h4 class="text-center" style="color:white;font-weight:bold;font-family:calibri">EAMCET/ ICET/ ECET/ POLYCET CODE: TCEK</h4>
+                    <div class="row">
+                        <div class="col-sm-6" style="background-color: #07294d;margin-top:-20px">
+                            <h4 class="text-center" style="color:white;font-weight:bold;font-family:calibri">EAMCET/ ICET/ ECET/ POLYCET CODE: TCEK</h4>
+                        </div>
+                        <div class="col-sm-6" style="background-color: #07294d;margin-top:-20px">
+                            <h4 class="text-center" style="color:white;font-weight:bold;font-family:calibri">For Admissions Contact : 8522954369, 7396903383</h4>
+                        </div>
                     </div>
+                    <!-- <div class="col-md-12 col-sm-12" style="background-color: #07294d;margin-top:-20px">
+                        <h4 class="text-center" style="color:white;font-weight:bold;font-family:calibri">EAMCET/ ICET/ ECET/ POLYCET CODE: TCEK</h4>
+                    </div> -->
                     <!-- end col -->
-                </div><!-- end row -->
-            </div><!-- end container -->
+                </div><!-- end row --> 
+            </div><!-- end container -->   
         </div><!-- end header-style-2 -->
