c5b34e21febf2945150ec9aacbdfb43674d9df83
srikanthyaswada <srikanthyaswada.vita@gmail.com>
2024-01-24 19:12:19 +0530

    timetable completed

---
+++ timetable.php
@@ -0,0 +1,49 @@
+<?php include 'header.php'; ?>
+<div class="row">
+    <div class="eee"
+        style="background-image: url('images/examination/time-table.jpg'); background-size:cover; height:350px; ">
+    </div>
+</div>
+
+<div class="container-fluid" style="box-shadow:-3px 5px 15px #000">
+    <ul class="nav nav-tabs navv" role="tablist" style="background-color: #f7f7f7" ;>
+        <li class="nav-item" style="color: rgb(0, 0, 255);">
+            <a class="nav-link active" data-toggle="tab" href="#home"><b
+                    style="font-size:16px;color:rgb(80, 80, 80)">Notifications
+                </b></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu1"><b style="font-size:16px;color:rgb(80, 80, 80)">Time
+                    Table
+                </b></a>
+        </li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div id="home" class="container tab-pane active" style="color:#082b2a;">
+
+        </div>
+
+
+        <div id="menu1" class="container tab-pane fade" style="color:#082b2a;">
+            <h3>The Examinations Time Table as per the directions of JNTU, Hyderabad.</h3><br>
+            <h4>B.TECH I YEAR END EXAMINATION SCHEDULES</h4>
+            <table class="table table-bordered">
+                <tr>
+                    <th>I YEAR B.TECH II SEMESTER – R18 REGULATIONS SUPPLEMENTARY EXAMINATIONS JUNE-2022</th>
+                    <td> <a href="files/timetable/I-YEAR-B.TECH-II-SEMESTER-–-R18-REGULATIONS-SUPPLEMENTARY-EXAMINATIONS-JUNE-2022.pdf"
+                            target="_new"> <button class="btn btn-primary">View</button></a> </td>
+                </tr>
+               
+                <tr>
+                    <th>B.TECH I YEAR I SEMESTER (CSE-ECE) – R18 REGULATIONS REGULAR-SUPPLY EXAMINATIONS JUNE-2022</th>
+                    <td> <a href="files/timetable/B.TECH-I-YEAR-I-SEMESTER-CSE-ECE-–-R18-REGULATIONS-REGULAR-SUPPLY-EXAMINATIONS-JUNE-2022.pdf"
+                            target="_new"> <button class="btn btn-primary">View</button></a> </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+<br><br>
+<?php include 'footer.php'; ?>
